cd282343f107ea97888715114a0f295e4fb688ef
Tammi Lee <tammilee219@gmail.com>
2019-08-12 12:35:09 -0700

    created Dojodachis class and instantiated an instance of it called myDojodachi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Dojodachi.Models;
+
+namespace Dojodachi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dojodachis myDojodachi = new Dojodachis();
+            myDojodachi.Fullness = 20;
+            myDojodachi.Happiness = 20;
+            myDojodachi.Energy = 50;
+            myDojodachi.Meals = 3;
+
+            return View(myDojodachi);
+        }
+
+        public IActionRestul Feed(){
+            if (myDojodachi.Meals > 0){
+                
+            }
+        }
+    }
+}
---
+++ Models/Dojodachis.cs
@@ -0,0 +1,8 @@
+namespace Dojodachi.Models{
+    public class Dojodachis{
+        public int Fullness {get;set;}
+        public int Happiness {get;set;}
+        public int Meals {get;set;}
+        public int Energy {get;set;}
+    }
+}
